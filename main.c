2cac75dadc3fa73ebf6e9b6b1fc3058e6b45d426
Dmitry V. Sokolov <ha@haqr.eu>
2020-02-02 21:46:02 +0100

    sweeping all three servos

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
 
 #define  INPUT2(port,pin)   DDR ## port &= ~_BV(pin)
 #define OUTPUT2(port,pin)   DDR ## port |=  _BV(pin)
@@ -19,20 +21,34 @@
 #define TEST_PIN       C,5
 #define SERVO_L_PIN    B,1
 #define SERVO_R_PIN    B,2
+#define SERVO_C_PIN    B,3
 
 // The servos take a 50 Hz PWM signal; 1 ms minimum pulse width (0 deg), 2 ms maximum pulse width (180 deg)
+
+
 
 int main(void) {
     OUTPUT(TEST_PIN);
     OUTPUT(SERVO_L_PIN);
     OUTPUT(SERVO_R_PIN);
+    OUTPUT(SERVO_C_PIN);
 
-    TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11); // FastPWM Mode mode TOP determined by ICR1 - non-inverting Compare Output mode
-    TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS11);    // set prescaler to 8, FastPWM Mode mode continued
-    ICR1 = 20000;      // set period to 20 ms
-    OCR1A = 1000;      // set count to 1 ms: 0 deg
-    OCR1B = 1000;      // set count to 1 ms: 0 deg
-    TCNT1 = 0;         // reset timer
+    { // init timer1
+        TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11); // fast PWM Mode mode TOP determined by ICR1 - non-inverting Compare Output mode
+        TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS11);    // set prescaler to 8, FastPWM Mode mode continued
+        ICR1 = 20000;      // set period to 20 ms
+        OCR1A = 1000;      // set count to 1 ms: 0 deg
+        OCR1B = 1000;      // set count to 1 ms: 0 deg
+        TCNT1 = 0;         // reset timer
+    }
+
+    { // init timer2
+        TCCR2 = (1<<WGM20)| (1<<WGM21) | (1<<COM21) | (1<<CS22) | (1<<CS20); // fast PWM
+        OCR2 = 1000/16; // set count to 1 ms: 0 deg
+    }
+
+    TIMSK |= (1<<TICIE1) | (1<<OCIE2); // initialize interrupts, activate
+    sei();
 
     uint8_t sweep_up = 1;
     while (1) {
@@ -44,8 +60,18 @@ int main(void) {
         if (!sweep_up && OCR1B<=1000) sweep_up = 1;
         OCR1A += (sweep_up?4:-4);
         OCR1B += (sweep_up?4:-4);
+        OCR2 = OCR1A/16;
     }
 
     return 0;
+}
+
+ISR(TIMER2_COMP_vect) { // overflow interrupt timer2
+    TCCR2 &= 0x00; // delete timer2
+}
+
+ISR(TIMER1_CAPT_vect) { // capture interrupt timer1
+    TCNT2 = 255;
+    TCCR2 = (1<<WGM20) | (1<<WGM21) |(1<<COM21) | (1<<CS22) | (1<<CS20); // set timer2
 }
 
